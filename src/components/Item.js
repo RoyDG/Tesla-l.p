b09809df9e0ec1c8fbf9862d03ae338bc509bd67
Roy <rdg01834@gmail.com>
2021-01-19 03:29:23 +0600

    item done

---
+++ src/components/Item.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Item.css'
+import Button from './Button'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+function Item({title,
+        desc,
+        descLink,
+        backgroundImg,
+        leftBtnTxt,
+        leftBtnLink,
+        rightBtnTxt,
+        rightBtnLink,
+        twoButtons,
+        first}) {
+    return (
+        <div className = "item" style = {{
+            backgroundImage: `url(${backgroundImg})`
+        }}>
+            <div className = 'item__container'>
+            <div className="item__text">
+                <p>{title}</p>
+                <div className="item__text__description">
+                    <p>{desc}</p>
+                </div>
+            </div>
+
+            <div className="item__bottom">
+                <div className="item__button">
+                    <Button imp = 'primary' text = {leftBtnTxt} link = {leftBtnLink}/>
+                    {
+                        twoButtons && (
+                            <Button imp = 'secondary' text = {rightBtnTxt} link = {rightBtnLink} />
+                        )
+                    }
+                </div>
+                {first && (
+                    <div className="item__expand">
+                        <ExpandMoreIcon/>
+                    </div>
+                )}
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Item
